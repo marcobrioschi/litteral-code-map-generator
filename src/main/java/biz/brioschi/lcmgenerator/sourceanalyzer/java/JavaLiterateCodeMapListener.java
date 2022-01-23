c628f31c5f361d18f43b6cbd5fc9a274a0f0ba00
Marco Brioschi <marco.brioschi@leroymerlin.it>
2022-01-23 01:29:43 +0100

    Code generalization

--- src/main/java/biz/brioschi/lcmgenerator/sourceanalyzer/java/JavaLiterateCodeMapListener.java
+++ src/main/java/biz/brioschi/lcmgenerator/sourceanalyzer/java/JavaLiterateCodeMapListener.java
@@ -4,7 +4,9 @@ import biz.brioschi.lcmgenerator.antlr.java.parser.JavaParser;
 import biz.brioschi.lcmgenerator.antlr.java.parser.JavaParserBaseListener;
 import biz.brioschi.lcmgenerator.diagram.BoxConnection;
 import biz.brioschi.lcmgenerator.diagram.LiterateCodeMapBox;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,53 +27,48 @@ public class JavaLiterateCodeMapListener extends JavaParserBaseListener {
 
     @Override
     public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
-        List<BoxConnection> connections = new ArrayList<>();
         String className = ctx.identifier().getText();
-        int lastReadedChild = 1;
-        ParseTree currentExtensionType = ctx.getChild(++lastReadedChild);
-        if (currentExtensionType == ctx.EXTENDS()) {
-            String targetBoxName = ctx.getChild(++lastReadedChild).getText();
-            connections.add(new BoxConnection(BoxConnection.ConnectionType.EXTENDS, targetBoxName));
-            currentExtensionType = ctx.getChild(++lastReadedChild);
-        }
-        if (currentExtensionType == ctx.IMPLEMENTS()) {
-            ParseTree interfaceList = ctx.getChild(++lastReadedChild);
-            connections.addAll(parseListOfExtensions(interfaceList));
-        }
+        List<BoxConnection> connections = getCurrentBoxExtensions(ctx);
         generateANewBoxElement(BoxType.JAVA_CLASS, className, connections);
     }
 
     @Override
     public void enterInterfaceDeclaration(JavaParser.InterfaceDeclarationContext ctx) {
-        List<BoxConnection> connections = new ArrayList<>();
         String className = ctx.identifier().getText();
-        ParseTree currentExtensionType = ctx.getChild(2);
-        if (currentExtensionType == ctx.EXTENDS()) {
-            ParseTree interfaceList = ctx.getChild(3);
-            connections.addAll(parseListOfExtensions(interfaceList));
-        }
+        List<BoxConnection> connections = getCurrentBoxExtensions(ctx);
         generateANewBoxElement(BoxType.JAVA_INTERFACE, className, connections);
     }
 
     @Override
     public void enterEnumDeclaration(JavaParser.EnumDeclarationContext ctx) {
-        List<BoxConnection> connections = new ArrayList<>();
         String className = ctx.identifier().getText();
-        ParseTree currentExtensionType = ctx.getChild(2);
-        if (currentExtensionType == ctx.IMPLEMENTS()) {
-            ParseTree interfaceList = ctx.getChild(3);
-            connections.addAll(parseListOfExtensions(interfaceList));
-        }
+        List<BoxConnection> connections = getCurrentBoxExtensions(ctx);
         generateANewBoxElement(BoxType.JAVA_ENUM, className, connections);
     }
 
-    private List<BoxConnection> parseListOfExtensions(ParseTree interfaceList) {
+    private List<BoxConnection> getCurrentBoxExtensions(ParserRuleContext ctx) {
         List<BoxConnection> connections = new ArrayList<>();
-        for (int i = 0; i < interfaceList.getChildCount(); ++i) {
-            String currentToken = interfaceList.getChild(i).getText();
-            if (!currentToken.equals(",")) {
-                connections.add(new BoxConnection(BoxConnection.ConnectionType.EXTENDS, currentToken));
-            }
+        TerminalNode extendsToken = ctx.getToken(JavaParser.EXTENDS, 0);
+        TerminalNode implementsToken = ctx.getToken(JavaParser.IMPLEMENTS, 0);
+        int lastReadedChild = 2;
+        while ((ctx.getChild(lastReadedChild) == extendsToken) || (ctx.getChild(lastReadedChild) == implementsToken)) {
+            ParseTree extendsChild = ctx.getChild(lastReadedChild + 1);
+            connections.addAll(parseListOfCurrentBoxExtensions(extendsChild));
+            lastReadedChild += 2;
+        };
+        return connections;
+    }
+
+    private List<BoxConnection> parseListOfCurrentBoxExtensions(ParseTree interfaceList) {
+        List<BoxConnection> connections = new ArrayList<>();
+        for (int i = 0; i < interfaceList.getChildCount(); i += 2) {
+            String currentExtensionName = interfaceList.getChild(i).getText();
+            connections.add(
+                    new BoxConnection(
+                            BoxConnection.ConnectionType.EXTENDS,
+                            currentExtensionName
+                    )
+            );
         }
         return connections;
     }
